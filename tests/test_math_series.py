ba4721eae15753d734ff1d82f50cf0ccd0d84a9d
RobertWRadford <robertr928@gmail.com>
2020-11-10 19:18:10 -0500

    updates

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -2,18 +2,18 @@ from math_series.math_series import fibonacci, lucas, sum_series
 
 def test_fibonacci():
 	actual = fibonacci(3)
-	expect = 5
+	expect = 2
 	assert actual == expect
 
-# def test_lucas():
-# 	actual = lucas(3)
-# 	expect = 4
-# 	assert actual == expect
+def test_lucas():
+	actual = lucas(3)
+	expect = 4
+	assert actual == expect
 
-# def test_sum_series():
-# 	actual = sum_series(3)
-# 	expect = 2
-# 	assert actual == expect, 'first assert'
-# 	actual = sum_series(2, 5, 3)
-# 	expect = 8
-# 	assert actual == expect, 'second assert'
+def test_sum_series():
+	actual = sum_series(3)
+	expect = 2
+	assert actual == expect, 'first assert'
+	actual = sum_series(2, 5, 3)
+	expect = 8
+	assert actual == expect, 'second assert'
